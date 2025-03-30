3a430c3989f3060f08577e269840382779ce5cd6
Philip <106172533+yankes404@users.noreply.github.com>
2025-03-30 21:53:11 +0200

    Removed bg from country image

--- src/components/country-image.tsx
+++ src/components/country-image.tsx
@@ -5,11 +5,11 @@ interface Props {
 
 export const CountryImage = ({ url, name }: Props) => {
     return (
-        <div className="w-full h-28 rounded-xl bg-neutral-100 border border-neutral-200 shadow-xs grid place-items-center overflow-hidden">
+        <div className="w-full h-28 grid place-items-center overflow-hidden">
             <img
                 src={url}
                 alt={`Flag of ${name}`}
-                className="h-20 rounded-md shadow"
+                className="h-20 rounded-md"
             />
         </div>
     );
