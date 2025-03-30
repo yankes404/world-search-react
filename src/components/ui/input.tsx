a0304c3b20ac35e48dd1ab85dadba499cd32fb24
Philip <106172533+yankes404@users.noreply.github.com>
2025-03-30 18:49:11 +0200

    Removed forwardRef

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -1,18 +1,12 @@
-import { forwardRef } from "react";
-
 import { cn } from "../../lib/utils";
 
-const Input = forwardRef<HTMLInputElement, React.InputHTMLAttributes<HTMLInputElement>>(({
-    className,
-    ...props
-}, ref) => (
-    <input
-        ref={ref}
-        className={cn("py-2 px-3 rounded-md shadow-sm border w-full bg-white border-neutral-200 text-sm text-black placeholder:text-neutral-600 outline-none focus-visible:shadow-md disabled:pointer-events-none disabled:opacity-50", className)}
-        {...props}
-    />
-))
-
-Input.displayName = "Input";
+function Input ({ className, ...props }: React.ComponentProps<"input">) {
+    return (
+        <input
+            className={cn("py-2 px-3 rounded-md shadow-sm border w-full bg-white border-neutral-200 text-sm text-black placeholder:text-neutral-600 outline-none focus-visible:shadow-md disabled:pointer-events-none disabled:opacity-50", className)}
+            {...props}
+        />
+    )
+}
 
 export { Input }
